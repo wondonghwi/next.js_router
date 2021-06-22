2ebd04a928dedfe674ce86adccbbf9208ebc311d
원동휘 <donghwi1289@gmail.com>
2021-06-22 23:50:35 +0900

    ADD : event관련 로직 ,파일 추가

--- components/event-list-item.js
+++ components/event-list-item.js
@@ -1,6 +1,9 @@
 import React from 'react';
-import Link from 'next/link';
 import classes from '/styles/event-item.module.css';
+import DateIcon from "../pages/icons/date-icon";
+import AddressIcon from "../pages/icons/address-icon";
+import ArrowRightIcon from "../pages/icons/arrow-right-icon";
+import Button from "../pages/ui/button";
 
 const EventListItem = ({ title, image, date, location, id }) => {
   const humanReadableDate = new Date(date).toLocaleDateString('en-US', {
@@ -19,14 +22,19 @@ const EventListItem = ({ title, image, date, location, id }) => {
         {/*<div className={classes.summary}>*/}
         <h2>TITLE</h2>
         <div className={classes.date}>
+          <DateIcon />
           <time>{humanReadableDate}</time>
         </div>
         <div className={classes.address}>
+          <AddressIcon />
           <address>{formattedAddress}</address>
         </div>
         {/*</div>*/}
         <div className={classes.actions}>
-          <Link href={expolreLink}>Explore</Link>
+          <Button link={expolreLink}>
+          <span>Explore</span>
+          <span className={classes.icon}><ArrowRightIcon /></span>
+          </Button>
         </div>
       </div>
     </li>
