0a1056df11e68946bb0b3edce7dbe371e937e43a
wondonghwi <donghwi1289@gmail.com>
2021-06-22 13:42:24 +0900

    ADD : 라우팅을 위한 파일경로 세팅

---
+++ pages/events/[...slug].js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const FilterdEventPage = () => {
+  return (
+    <div>
+      <h1>Filtered Event Page</h1>
+    </div>
+  );
+};
+
+export default FilterdEventPage;
---
+++ pages/events/[eventId].js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const EventDetailPage = () => {
+  return (
+    <div>
+      <h1>Event DetailPage</h1>
+    </div>
+  );
+};
+
+export default EventDetailPage;
